5c83f089113b60f0198469054d661dce616af1f5
Ben S <bencraft1392@gmail.com>
2019-11-14 12:39:49 -0500

    Added notes to C++/Ch1/pStruct

--- cpp-crash/chapter-1/prgm-struct/main.cpp
+++ cpp-crash/chapter-1/prgm-struct/main.cpp
@@ -1,6 +1,24 @@
+// INCLUDE: PULLS IN CODE FROM AN EXT LIBRARY
+// Importing libraries keeps you from having to "reinvent the wheel" when coding.
+// By including a prewritten set of code, you save time and effort.
+// Here, the Standard C IO Library is imported.
 #include <cstdio>
 
+// MAIN: A C++ PROGRAM'S STARTING POINT
+// This function, main(), is the entry point for c++ programs.
+// An entry point is a function that executes when the user runs a program.
+// A function is a block of code that can take inputs, execute instructions, and return an output.
 int main() {
+
+    // PRINTF: A FUNCTION, PRINTS TO THE CONSOLE
+    // This prints characters to the console.
+    // In this case, it prints "Hello, World!"
     printf("Hello, World!");
+
+    // RETURN: EXITS A FUNCTION, RETURNS A VALUE
+    // Here return is used to exit the program.
+    // In this case, it also sends an exit code, 0, to the operating system.
+    // This code (usually 0 when successful) tells the OS how well the program ran.
     return 0;
+
 }
