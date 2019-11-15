8e86269c5f8f4b0507a8800d93f8f6e0310cd91d
Ben S <bencraft1392@gmail.com>
2019-11-15 12:40:55 -0500

    Started notes on C++/Ch2/FT

---
+++ cpp-crash/chapter-2/fundamental/main.cpp
@@ -0,0 +1,11 @@
+// Library imports.
+#include <cstdio>
+
+// INTEGER TYPES
+// These store whole numbers.
+// Short/[Normal:Int]/Long/LongLong = 2/4/4/8 Bytes
+// [Normal:Signed]/Unsigned = Can be Negative / Cannot be Negative
+short a = -32768; unsigned short b = 65536;
+int c = -2147483648; unsigned int d = 4294967296;
+long e = -2147483648; unsigned long f = 4294967296;
+long long g = -9223372036854776000; unsigned long long h = 18446744073709552000;
