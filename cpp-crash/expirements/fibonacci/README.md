070b23f8720ae22da7c180478764b6784be1b802
Ben S <bencraft1392@gmail.com>
2019-11-20 13:46:49 -0500

    Made working fibbonichi.

--- cpp-crash/expirements/fibonacci/README.md
+++ cpp-crash/expirements/fibonacci/README.md
@@ -5,3 +5,9 @@ Fibonacci sequence test.
 
 ### Files
 - **[main.cpp](main.cpp)**: All code.
+- **[test.csv](test.csv)**: Example file generated from `stdout`.
+
+---
+
+### Notes
+When compiled, the executable will output the sequence to `stdout` in CSV format so you can use it in a graph. An example of a file generated is in [test.csv](test.csv).
--- cpp-crash/expirements/fibonacci/main.cpp
+++ cpp-crash/expirements/fibonacci/main.cpp
@@ -2,7 +2,26 @@
 
 int main() {
 
-    printf("Hello");
+    // Define # of terms to generate.
+    const unsigned long long n = 94;
+
+    // Initialize variables.
+    unsigned long long t1 = 0;
+    unsigned long long t2 = 1;
+    unsigned long long tn = 0;
+
+    // Generate the sequence.
+    printf("Fibonacci\n%llu\n%llu", t1, t2);
+    for (int i = 3; i <= n; i++) {
+
+        tn = t1 + t2;
+        t1 = t2;
+        t2 = tn;
+
+        printf("\n%llu", tn);
+
+    }
+    printf("\n");
 
     return 0;
 
